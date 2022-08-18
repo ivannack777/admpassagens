0782cc37ddbbc4ad0406f2204bb75092c2463557
Ivan Nack <ivan.nack@grupoconstruserv.eng.br>
2022-08-18 15:44:30 -0300

    Adicionado colunas tipo_dispositivos e estado

--- src/Application/Models/GrupoDispositivoAmbiente.php
+++ src/Application/Models/GrupoDispositivoAmbiente.php
@@ -20,10 +20,14 @@ class GrupoDispositivoAmbiente extends \Illuminate\Database\Eloquent\Model
         $grupo_dispositivo_ambientes->select(
             'grupo_dispositivo_ambiente.id',
             'grupo_dispositivo_ambiente.ambiente_id',
+            'grupo_dispositivo_ambiente.dispositivo_tipo_id',
             'grupo_dispositivo_ambiente.nome',
+            'grupo_dispositivo_ambiente.estado',
+            'dispositivo_tipo.nome as tipo_dispositivo',
             'ambiente.nome as ambiente_nome',
         );
         $grupo_dispositivo_ambientes->join('ambiente', 'ambiente.id','=','grupo_dispositivo_ambiente.ambiente_id', 'left');
+        $grupo_dispositivo_ambientes->join('dispositivo_tipo', 'dispositivo_tipo.id','=','grupo_dispositivo_ambiente.dispositivo_tipo_id', 'left');
         foreach($params as $campo => $param){
             if($campo == 'dispositivo.nome'){
                 $grupo_dispositivo_ambientes->where($campo, 'like', "%{$param}%");
